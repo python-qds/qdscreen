d5898e0336086c3e094539475f312254e5a8f53a
Sylvain MARIE <sylvain.marie@se.com>
2021-01-09 00:30:39 +0100

    First complete version returning a forest and supporting quasi-determinism. Added a few unit tests concerning sub routines

--- qdsbnsl/main.py
+++ qdsbnsl/main.py
@@ -1,8 +1,11 @@
-from copy import copy
-
 import numpy as np
 import pandas as pd
 from pyitlib import discrete_random_variable as drv
+
+try:
+    from typing import Union
+except:  # noqa
+    pass
 
 
 def qdeterscreen(df,                     # type: pd.DataFrame
@@ -18,34 +21,39 @@ def qdeterscreen(df,                     # type: pd.DataFrame
 
     if epsilon_absolute is None:
         if epsilon_relative is None:
+            # if nothing is provided, use absolute threshold 0
             epsilon_absolute = 0
             is_absolute = True
         else:
+            # relative threshold provided
             is_absolute = False
     else:
+        if epsilon_relative is not None:
+            raise ValueError("only one of absolute and relative should be passed")
+        # absolute threshold provided
         is_absolute = True
 
+    is_strict = (epsilon_absolute == 0.) if is_absolute else (epsilon_relative == 0.)
+
+    # sanity check
     if is_absolute and epsilon_absolute < 0:
         raise ValueError("epsilon_absolute should be positive")
     elif not is_absolute and (epsilon_relative < 0 or epsilon_relative > 1):
         raise ValueError("epsilon_relative should be 0=<eps=<1")
 
     # (0) compute conditional entropies or relative conditional entropies
-    H_df = compute_conditional_entropies(df, is_absolute)
-
-    # (1) initial adjacency matrix (transpose because H is the other way round)
-    A_df_orig = pd.DataFrame((H_df.values == 0).T, index=list(H_df.columns), columns=list(H_df.columns))
+    A_df_orig, df_stats = get_adjacency_matrix(df, eps_absolute=epsilon_absolute, eps_relative=epsilon_relative)
 
     # (2) identify redundancy
-    if (is_absolute and epsilon_absolute == 0) or (not is_absolute and epsilon_relative == 0):
-        A_df_noredundancy = identify_redundancy_strict(A_df_orig)
-    else:
-        # TODO implement
-        A_df_noredundancy = identify_redundancy_quasi(A_df_orig)
+    A_df_noredundancy = remove_redundancies(A_df_orig, selection_order=None if is_strict else df_stats.entropy_order_desc)
 
-    # (3) transform into forest: remove extra parents
-    parent_scores = compute_nb_levels(df)
-    A_df_forest = to_forest(A_df_noredundancy, parent_scores)
+    # (3) transform into forest: remove extra parents by keeping only the parent with lowest entropy / nb levels ?
+    # if X -> Z and X -> Y and Y -> Z then Z has two parents X and Y but only Y should be kept
+    # Determinist case: minimize the number of parameters: take the minimal nb of levels
+    # Quasi-determinist case: take the lowest entropy
+    entropy_order_inc = (df_stats.H.values).argsort()
+    # parent_order = compute_nb_levels(df) if is_strict else entropy_based_order
+    A_df_forest = to_forest(A_df_noredundancy, entropy_order_inc)
 
     # forestAdjMatList <- FromHToDeterForestAdjMat(H = H, criterion = criterionNlevels, removePerfectMatchingCol = removePerfectMatchingCol)
 
@@ -82,38 +90,152 @@ def qdeterscreen(df,                     # type: pd.DataFrame
     #     print("Final graph computed")
     #   }
 
-    return adjmat
+    return A_df_forest
 
 
-def compute_conditional_entropies(df, is_absolute):
+class Entropies(object):
+    """ to do this could be easier to read with pyfields and default value factories """
+
+    __slots__ = ('dataset', 'is_nparray', '_H', '_Hcond', '_Hcond_rel')
+
+    def __init__(self, df_or_array):
+        """
+
+        :param df_or_array: a pandas dataframe or numpy array where the variables are the columns
+        """
+        self.dataset = df_or_array
+        self.is_nparray = isinstance(df_or_array, np.ndarray)
+        self._H = None          # type: Union[np.ndarray, pd.Series]
+        self._Hcond = None      # type: Union[np.ndarray, pd.DataFrame]
+        self._Hcond_rel = None  # type: Union[np.ndarray, pd.DataFrame]
+
+    @property
+    def varnames(self):
+        if self.is_nparray:
+            raise ValueError("Variable names not available")
+        return list(self.dataset.columns)
+
+    @property
+    def H(self):
+        """The entropies of all variables. a pandas Series if df was a pandas dataframe, else a 1D numpy array"""
+        if self._H is None:
+            # Using pyitlib to compute H (hopefully efficiently)
+            self._H = drv.entropy(self.dataset.T)
+            if not self.is_nparray:
+                self._H = pd.Series(self._H, index=self.varnames)
+
+            # basic sanity check: should all be positive
+            assert np.all(self._H >= 0)
+        return self._H
+
+    @property
+    def Hcond(self):
+        """
+        The conditional entropy matrix (i, j) = H(Xi | Xj).
+        A pandas Dataframe or a 2D numpy array depending on dataset type
+        """
+        if self._Hcond is None:
+            # Old attempt to do it ourselves
+            # (0) init H
+            # nb_vars = len(df.columns)
+            # H = np.empty((nb_vars, nb_vars), dtype=float)
+            # (1) for each column compute the counts per value
+            # (2) for each (i, j) pair compute the counts
+
+            # Using pyitlib to compute H (hopefully efficiently)
+            self._Hcond = drv.entropy_conditional(self.dataset.T)
+
+            # add the row/column headers
+            if not self.is_nparray:
+                self._Hcond = pd.DataFrame(self._Hcond, index=self.varnames, columns=self.varnames)
+
+            # basic sanity check: should all be positive
+            assert np.all(self._Hcond >= 0)
+        return self._Hcond
+
+    @property
+    def Hcond_rel(self):
+        """Relative conditional entropies of row vs. col. """
+        if self._Hcond_rel is None:
+            # compute relative entropy: in each cell (X, Y) we want H(X|Y)/H(X)
+            if self.is_nparray:
+                self._Hcond_rel = (self.Hcond.T / self.H).T
+            else:
+                Hrel_array = (self.Hcond.values.T / self.H.values).T
+                self._Hcond_rel = pd.DataFrame(Hrel_array, index=list(self.Hcond.columns), columns=list(self.Hcond.columns))
+
+            # basic sanity check
+            assert np.all(self._Hcond_rel >= 0.)
+            assert np.all(self._Hcond_rel <= 1.)
+        return self._Hcond_rel
+
+    @property
+    def entropy_order_desc(self):
+        if self.is_nparray:
+            return (-self.H).argsort()
+        else:
+            return (-self.H.values).argsort()
+
+    @property
+    def entropy_order_asc(self):
+        if self.is_nparray:
+            return self.H.argsort()
+        else:
+            return self.H.values.argsort()
+
+
+def get_adjacency_matrix(df,                 # type: Union[np.ndarray, pd.DataFrame]
+                         eps_absolute=None,  # type: float
+                         eps_relative=None   # type: float
+                         ):
     """
-    R function: MiMatrix
 
-    :param is_absolute:
-    :return: a conditional entropy matrix where element (i, j) is H(Xi|Xj)
+    :param df:
+    :param eps_absolute:
+    :param eps_relative:
+    :return:
     """
-    # (0)init H
-    # nb_vars = len(df.columns)
-    # H = np.empty((nb_vars, nb_vars), dtype=float)
+    df_stats = Entropies(df)
 
-    # (1) for each column compute the counts per value
+    # (1) create initial adjacency matrix by thresholding either H(X|Y) (absolute) or H(X|Y)/H(X) (relative)
+    if eps_relative is None:
+        # default value for eps absolute
+        if eps_absolute is None:
+            eps_absolute = 0
 
-    # (2) for each (i, j) pair compute the counts
+        # threshold is on H(X|Y)
+        edges = (df_stats.Hcond.values <= eps_absolute).T
+    else:
+        if eps_absolute is not None:
+            raise ValueError("Only one of `eps_absolute` and `eps_relative` should be provided")
 
-    # compute H efficiently (hopefully)
-    H = drv.entropy_conditional(df.T)
+        # threshold on H(X|Y)/H(X)
+        edges = (df_stats.Hcond_rel.values <= eps_relative).T
 
-    # add the row/column headers
-    H_df = pd.DataFrame(H, index=list(df.columns), columns=list(df.columns))
+    # the diagonal should be false
+    np.fill_diagonal(edges, False)
 
-    return H_df
+    # finally create the matrix
+    A_df = pd.DataFrame(edges, index=df_stats.varnames, columns=df_stats.varnames)
+
+    return A_df, df_stats
 
 
-def identify_redundancy_strict(A_df):
-    """
+def remove_redundancies(A_df,
+                        selection_order=None
+                        ):
+    """Cleans the arcs in A_df between redundant variables.
 
-    :param A_df:
-    :return: an adjacency matrix where A(i, j) indicates that there is an arc i -> j
+    A_df should be a dataframe where index = columns and indicate the node name.
+    It should contain boolean values where True at (i,j) means there is a directed arc i -> j
+
+    When there are redundant variables (arcs between i->j and j->i), only a single node is kept as the parent
+    of all other redundant nodes.
+
+    :param A_df: an adjacency matrix where A(i, j) indicates that there is an arc i -> j
+    :param selection_order: if none (default) the first node in the list will be kept as representant for each
+        redundancy class. Otherwise an alternate order (array of indices) can be provided.
+    :return:
     """
     # work on a copy
     A_df = A_df.copy(deep=True)
@@ -121,11 +243,17 @@ def identify_redundancy_strict(A_df):
     # init
     A = A_df.values
     n_vars = A_df.shape[0]
+    if selection_order is None:
+        selection_order = range(n_vars)
 
     # I contains the list of variable indices to go through. We can remove some in the loop
     I = np.ones((n_vars, ), dtype=bool)
-    for i in range(n_vars):
+
+    # for each node
+    for i in selection_order:
+        # if we did not yet take it into account
         if I[i]:
+            # find redundant nodes with this node (= there is an arc in both directions)
             mask_redundant_with_i = A[i, :] * A[:, i]
 
             # we will stop caring about this redundancy class
@@ -146,40 +274,65 @@ def identify_redundancy_strict(A_df):
                 # Note that this destroys the diagonal but we will fix this at the end
                 A[redundant_nodes_idx, redundant_nodes_idx] = False
 
-    # restore the diagonal
-    np.fill_diagonal(A, True)
+    # restore the diagonal: no
+    # np.fill_diagonal(A, True)
 
     return A_df
 
 
-def to_forest(A_df, parent_scores=None, criterion_func=None):
-    """
+def to_forest(A_df, selection_order, inplace=False):
+    """ Removes extra arcs in the adjacency matrix Z_df by only keeping the first parent in the given order
 
     :param A_df:
-    :param parent_scores:
-    :param criterion_func: if parent_scores is a
     :return:
     """
-    A_df = A_df.copy(deep=True)
+    if not inplace:
+        A_df = A_df.copy()
 
-    if parent_scores is not None and criterion_func is not None:
-        raise ValueError("only one of `parent_scores` or `criterion_func` should be provided")
+    # nb_parents = A_df.sum(axis=0)
+    # nodes_with_many_parents = np.where(nb_parents.values > 1)[0]
 
-    elif parent_scores is not None:
-        # for each child (column) we want to keep only a single parent
-        if parent_scores.shape != (A_df.shape[0], ):
-            raise ValueError("shape of `parent_scores` does not match")
+    # Probably the fastest: when the cumulative nb of parents is above 1 they need to be removed
+    A_df[A_df.iloc[selection_order, :].cumsum(axis=0) > 1] = False
 
-        A_df * parent_scores
+    # # Probably slower but easier to understand
+    # # for each column we want to only keep one (or zero) row (parent) with True
+    # for i in range(A_df.shape[1]):
+    #     first_parent_idx = A_df.values[selection_order, i].argmax(axis=0)
+    #     A_df.values[selection_order[first_parent_idx+1:], i] = False
 
-    elif criterion_func is not None:
-        # TODO use criterion_func(i, j) to get the score
-        raise NotImplementedError()
+    if not inplace:
+        return A_df
 
-    else:
-        raise ValueError("at least one of `parent_scores` or `criterion_func` should be provided")
 
-    return A_df
+# def to_forest(A_df, parent_scores=None, criterion_func=None):
+#     """
+#
+#     :param A_df:
+#     :param parent_scores:
+#     :param criterion_func: if parent_scores is a
+#     :return:
+#     """
+#     A_df = A_df.copy(deep=True)
+#
+#     if parent_scores is not None and criterion_func is not None:
+#         raise ValueError("only one of `parent_scores` or `criterion_func` should be provided")
+#
+#     elif parent_scores is not None:
+#         # for each child (column) we want to keep only a single parent
+#         if parent_scores.shape != (A_df.shape[0], ):
+#             raise ValueError("shape of `parent_scores` does not match")
+#
+#         A_df * parent_scores
+#
+#     elif criterion_func is not None:
+#         # TODO use criterion_func(i, j) to get the score
+#         raise NotImplementedError()
+#
+#     else:
+#         raise ValueError("at least one of `parent_scores` or `criterion_func` should be provided")
+#
+#     return A_df
 
 
 def compute_nb_levels(df):
--- qdsbnsl/tests/test_walkthrough.py
+++ qdsbnsl/tests/test_walkthrough.py
@@ -1,6 +1,129 @@
+import numpy as np
 import pandas as pd
+import pytest
 
+from qdsbnsl.main import get_adjacency_matrix, remove_redundancies, Entropies
 from qdsbnsl import qdeterscreen
+
+
+def df_strict1():
+    """ A dataframe with two equivalent redundant variables X and Y """
+    df = pd.DataFrame({
+        'X': ["a", "a", "b", "b", "a", "c", "c", "a", "b", "c"],
+    })
+    # 'Y':   ["b", "b", "c", "c", "b", "a", "a", "b", "c", "a"],
+    df['Y'] = df['X'].replace("c", "d").replace("b", "c").replace("a", "b").replace("d", "a")
+    return df
+
+
+def test_adjacency_strict():
+    """ Tests that `get_adjacency_matrix` works as expected in front of a dataset with redundancy """
+
+    # Compute the adjacency matrix
+    adj_df, df_stats = get_adjacency_matrix(df_strict1())
+    pd.testing.assert_frame_equal(adj_df, pd.DataFrame(data=[
+        [False, True],
+        [True, False]
+    ], index=['X', 'Y'], columns=['X', 'Y']))
+
+
+def quasi_df1():
+    # in that matrix the H(X|Y) = H(Y|X) = 0.324511 and the relative H(X|Y)/H(X) = H(Y|X)/H(Y) = 0.20657
+    return pd.DataFrame({
+        'X': ["a", "a", "b", "b", "a", "c", "c", "a", "b", "c"],
+        'Y': ["a", "b", "c", "c", "b", "a", "a", "b", "c", "a"],
+    })
+
+
+def test_adjacency_invalid_thresholds():
+    # an error is raised if both are provided, even if both are zero
+    with pytest.raises(ValueError):
+        get_adjacency_matrix(pd.DataFrame(), eps_absolute=0., eps_relative=0.)
+
+
+@pytest.mark.parametrize("eps_absolute, eps_relative", [
+    (None, None),  # strict mode
+    (0.2, None),   # too low absolute
+    (None, 0.15)   # too low relative
+])
+def test_adjacency_quasi_low_threshold(eps_absolute, eps_relative):
+    """ Tests that `get_adjacency_matrix` works as expected in quasi mode """
+
+    # in that matrix the H(X|Y) = H(Y|X) = 0.324511 and the relative H(X|Y)/H(X) = H(Y|X)/H(Y) = 0.20657
+    # strict mode or with too low threshold: no arc is detected
+    adj_df, df_stats = get_adjacency_matrix(quasi_df1(), eps_absolute=eps_absolute, eps_relative=eps_relative)
+    pd.testing.assert_frame_equal(adj_df, pd.DataFrame(data=[
+        [False, False],
+        [False, False]
+    ], index=['X', 'Y'], columns=['X', 'Y']))
+
+
+@pytest.mark.parametrize("eps_absolute, eps_relative", [
+    (0.33, None),  # high enough absolute
+    (None, 0.21)   # high enough relative
+])
+def test_adjacency_quasi_high_threshold(eps_absolute, eps_relative):
+    """ Tests that `get_adjacency_matrix` works as expected in quasi mode """
+
+    # in that matrix the H(X|Y) = H(Y|X) = 0.324511 and the relative H(X|Y)/H(X) = H(Y|X)/H(Y) = 0.20657
+    # strict mode or with too low threshold: no arc is detected
+    adj_df, df_stats = get_adjacency_matrix(quasi_df1(), eps_absolute=eps_absolute, eps_relative=eps_relative)
+    pd.testing.assert_frame_equal(adj_df, pd.DataFrame(data=[
+        [False, True],
+        [True, False]
+    ], index=['X', 'Y'], columns=['X', 'Y']))
+
+
+def test_remove_redundancies():
+    """ Tests that the redundancies removal routine works as expected """
+
+    # an adjacency matrix with two redundant nodes X and Y
+    adj_df = pd.DataFrame(data=[
+        [False, True],
+        [True, False]
+    ], index=['X', 'Y'], columns=['X', 'Y'])
+
+    # clean the redundancies with natural order: X is the representative of the class
+    adj_df_clean1 = remove_redundancies(adj_df)
+    pd.testing.assert_frame_equal(adj_df_clean1, pd.DataFrame(data=[
+        [False, True],
+        [False, False]
+    ], index=['X', 'Y'], columns=['X', 'Y']))
+
+    # clean the redundancies with reverse order: Y is the representative of the class
+    adj_df_clean2 = remove_redundancies(adj_df, selection_order=[1, 0])
+    pd.testing.assert_frame_equal(adj_df_clean2, pd.DataFrame(data=[
+        [False, False],
+        [True, False]
+    ], index=['X', 'Y'], columns=['X', 'Y']))
+
+
+# def test_identify_redundancy_quasi():
+#     df = pd.DataFrame({
+#         'U': ["a", "b", "d", "a", "b", "c", "a", "b", "d", "c"],
+#     })
+#     df['V'] = df['U'].replace("d", "c")  # d -> c
+#     df['W'] = df['V'].replace("c", "b")  # c -> b
+#
+#     adj_df = get_adjacency_matrix(df)
+#     df2 = identify_redundancy(adj_df)
+
+
+def df_mix1():
+    df = pd.DataFrame({
+        'U': ["a", "b", "d", "a", "b", "c", "a", "b", "d", "c"],
+        'V': ["a", "b", "c", "a", "b", "c", "a", "b", "c", "c"],
+        'W': ["a", "b", "b", "a", "b", "b", "a", "b", "b", "b"],
+        'X': ["a", "a", "b", "b", "a", "c", "c", "a", "b", "c"],
+        'Y': ["b", "b", "c", "c", "b", "a", "a", "b", "c", "a"],
+        'Z': ["a", "a", "b", "a", "a", "b", "b", "a", "a", "b"]
+    })
+
+    # In this data: the following relationships hold:
+    # H(W|V) = 0     H(W|U) = 0  H(V|U) = 0                   # U -> V -> W
+    # H(Y|X) = H(X|Y) = 0             # Y <-> X
+    # H(Z|Y) = H(Z|X) = 0.275 AND H(Z|Y) / H(Z) = 0.284  # X (et pas Y car il n'est pas représentant) -> Z si threshold qd > 0.19 (absolu) ou 0.28 (relatif)
+    return df
 
 
 def test_walkthrough():
@@ -13,17 +136,26 @@ def test_walkthrough():
     #
     # TODO the best is maybe   https://github.com/ncullen93/pyGOBN
 
-    df = pd.DataFrame({
-        'V': ["a", "b", "c", "a", "b", "c", "a", "b", "c", "c"],
-        'W': ["a", "b", "b", "a", "b", "b", "a", "b", "b", "b"],
-        'X': ["a", "a", "b", "b", "a", "c", "c", "a", "b", "c"],
-        'Y': ["b", "b", "c", "c", "b", "a", "a", "b", "c", "a"],
-        'Z': ["a", "a", "b", "a", "a", "b", "b", "a", "a", "b"]
-    })
+    df = df_mix1()
+    var_names = list(df.columns)
+    nb_vars = len(var_names)
 
-    # In this data: the following relationships hold:
-    # H(W|V) = 0
-    # H(Y|X) = H(X|Y) = 0
-    # H(Z|Y) = H(Z|X) = 0.19 AND H(Z|Y) / H(Z) = 0.28
+    df_stats = Entropies(df)
 
+    # strict
     adj_mat = qdeterscreen(df)
+    ref = pd.DataFrame(data=np.zeros((nb_vars, nb_vars), dtype=bool), index=var_names, columns=var_names)
+    ref.loc['U', 'V'] = True
+    ref.loc['V', 'W'] = True
+    ref.loc['X', 'Y'] = True
+    pd.testing.assert_frame_equal(adj_mat, ref)
+
+
+    # quasi
+    ref.loc['X', 'Z'] = True
+    # Z si threshold qd > 0.28 (absolu) ou 0.29 (relatif)
+    adj_mat = qdeterscreen(df, epsilon_absolute=0.28)
+    pd.testing.assert_frame_equal(adj_mat, ref)
+
+    adj_mat = qdeterscreen(df, epsilon_relative=0.29)
+    pd.testing.assert_frame_equal(adj_mat, ref)
